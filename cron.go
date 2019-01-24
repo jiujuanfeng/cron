7543deb39bf07703d3b4e509e0450d60de3a233e
jiujuanfeng <jiujuanfeng@163.com>
2019-01-25 01:47:10 +0800

    add func: removeJob

--- cron.go
+++ cron.go
@@ -50,6 +50,9 @@ type Entry struct {
 
 	// The Job to run.
 	Job Job
+
+	// Unique name to identify the Entry so as to be able to remove it later.
+	Name string
 }
 
 // byTime is a wrapper for sorting the entry array by time
@@ -82,7 +85,8 @@ func NewWithLocation(location *time.Location) *Cron {
 		entries:  nil,
 		add:      make(chan *Entry),
 		stop:     make(chan struct{}),
-		snapshot: make(chan []*Entry),
+		remove:   make(chan string),
+		snapshot: make(chan entries),
 		running:  false,
 		ErrorLog: nil,
 		location: location,
@@ -95,27 +99,57 @@ type FuncJob func()
 func (f FuncJob) Run() { f() }
 
 // AddFunc adds a func to the Cron to be run on the given schedule.
-func (c *Cron) AddFunc(spec string, cmd func()) error {
-	return c.AddJob(spec, FuncJob(cmd))
+func (c *Cron) AddFunc(spec string, cmd func(), name string) error {
+	return c.AddJob(spec, FuncJob(cmd), name)
 }
 
 // AddJob adds a Job to the Cron to be run on the given schedule.
-func (c *Cron) AddJob(spec string, cmd Job) error {
+func (c *Cron) AddJob(spec string, cmd Job, name string) error {
 	schedule, err := Parse(spec)
 	if err != nil {
 		return err
 	}
-	c.Schedule(schedule, cmd)
+	c.Schedule(schedule, cmd, name)
 	return nil
 }
 
+// RemoveJob removes a Job from the Cron based on name.
+func (c *Cron) RemoveJob(name string) {
+	if !c.running {
+		i := c.entries.pos(name)
+
+		if i == -1 {
+			return
+		}
+
+		c.entries = c.entries[:i+copy(c.entries[i:], c.entries[i+1:])]
+		return
+	}
+
+	c.remove <- name
+}
+
+func (entrySlice entries) pos(name string) int {
+	for p, e := range entrySlice {
+		if e.Name == name {
+			return p
+		}
+	}
+	return -1
+}
+
 // Schedule adds a Job to the Cron to be run on the given schedule.
-func (c *Cron) Schedule(schedule Schedule, cmd Job) {
+func (c *Cron) Schedule(schedule Schedule, cmd Job, name string) {
 	entry := &Entry{
 		Schedule: schedule,
 		Job:      cmd,
+		Name:     name,
 	}
 	if !c.running {
+		i := c.entries.pos(entry.Name)
+		if i != -1 {
+			return
+		}
 		c.entries = append(c.entries, entry)
 		return
 	}
@@ -205,10 +239,24 @@ func (c *Cron) run() {
 				}
 
 			case newEntry := <-c.add:
+				i := c.entries.pos(newEntry.Name)
+				if i != -1 {
+					break
+				}
+
 				timer.Stop()
 				now = c.now()
 				newEntry.Next = newEntry.Schedule.Next(now)
 				c.entries = append(c.entries, newEntry)
+			
+			case name := <-c.remove:
+				i := c.entries.pos(name)
+
+				if i == -1 {
+					break
+				}
+
+				c.entries = c.entries[:i+copy(c.entries[i:], c.entries[i+1:])]
 
 			case <-c.snapshot:
 				c.snapshot <- c.entrySnapshot()
